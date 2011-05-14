70aad7e7832a24c6bcae126be3dabae518d9616e
Nils Werner <nils.werner@gmail.com>
2011-05-14 14:00:55 +0200

    Eigene Namen etc.

--- depends.js
+++ depends.js
@@ -1,5 +1,5 @@
 /* Copyright 2009-2011 Hewlett-Packard Development Company, L.P. All rights reserved. */
 enyo.depends(
-	"source/MultiPanel.js",
-	"source/MultiPanel.css"
+	"source/EditorPanel.js",
+	"source/EditorPanel.css"
 );
--- source/EditorPanel.js
+++ source/EditorPanel.js
@@ -1,6 +1,6 @@
 /* Copyright 2009-2011 Hewlett-Packard Development Company, L.P. All rights reserved. */
 enyo.kind({
-	name: "MultiPanel",
+	name: "EditorPanel",
 	kind: enyo.VFlexBox,
 	components: [
 		{name: "slidingPane", kind: "SlidingPane", flex: 1, components: [
