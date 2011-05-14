fc45bc3064167e4844b8b3c6488f5b10397c3dee
Nils Werner <nils.werner@gmail.com>
2011-05-14 14:02:49 +0200

    SlidingPane und SlidingView ruebergeholt

--- depends.js
+++ depends.js
@@ -1,5 +1,7 @@
 /* Copyright 2009-2011 Hewlett-Packard Development Company, L.P. All rights reserved. */
 enyo.depends(
+	"source/SlidingPane.js",
+	"source/SlidingView.js",
 	"source/EditorPanel.js",
 	"source/EditorPanel.css"
 );
