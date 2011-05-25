18f7b07eb0f609e86865cbb2d5dd5742e58e2a9c
Nils Werner <nils.werner@gmail.com>
2011-05-25 16:51:38 +0200

    Ein bischen restyled

--- depends.js
+++ depends.js
@@ -1,10 +1,13 @@
 /* Copyright 2009-2011 Hewlett-Packard Development Company, L.P. All rights reserved. */
 enyo.depends(
-	"source/HSlidingPane.js",
-	"source/HSlidingView.js",
+	"source/kinds/HSlidingPane.js",
+	"source/kinds/HSlidingView.js",
 	"source/kinds/Label.js",
+	
 	"source/showdown/compressed/showdown.js",
+	
 	"$enyo-lib/printdialog/",
+	
 	"source/EditorPanel.js",
 	"source/EditorPanel.css"
 );
