a8703bc2679ac0887559e905fdc78660a59f57d3
Nils Werner <nils.werner@gmail.com>
2011-05-26 19:36:56 +0200

    Scroller in Popups

--- source/kinds/typewriterHelper.js
+++ source/kinds/typewriterHelper.js
@@ -1,38 +1,45 @@
 enyo.kind({
 	name: 'typewriterHelper',
-	kind: "Popup",
-	defaultKind:"Item",
+	kind: enyo.Popup,
 	components: [
-		{className: "enyo-first", components: [
-			{kind: "HFlexBox", components: [
-				{className: "markdownHelper", flex: 1, content: ""+
+		{kind: "BasicScroller",
+			autoHorizontal: false,
+			horizontal: false,
+			defaultKind: "Item",
+			className: "enyo-first enyo-last",
+			components: [
+				{className: "enyo-first", components: [
+					{kind: "HFlexBox", components: [
+						{className: "markdownHelper", flex: 1, content: ""+
+							"Title of Work\n" + 
+							"=============\n" +
+							"\n" +
+							"This text is styled to resemble a cover page\n" +
+							"\n" +
+							"----\n" +
+							"\n" +
+							"The horizontal ruler (above) makes this become regular text"
+						},
+						{kind: "Label", content:"Cover Page"}
+					]}
+				]},
+				{className: "enyo-last", components: [
+					{kind: "HFlexBox", components: [
+						{className: "markdownHelper", flex: 1, content: ""+
 					"Title of Work\n" + 
 					"=============\n" +
 					"\n" +
 					"This text is styled to resemble a cover page\n" +
 					"\n" +
-					"----\n" +
+					"First Chapter\n" +
+					"-------------\n" +
 					"\n" +
-					"The horizontal ruler (above) makes this become regular text"
-				},
-				{kind: "Label", content:"Cover Page"}
-			]}
-		]},
-		{className: "enyo-last", components: [
-			{kind: "HFlexBox", components: [
-				{className: "markdownHelper", flex: 1, content: ""+
-			"Title of Work\n" + 
-			"=============\n" +
-			"\n" +
-			"This text is styled to resemble a cover page\n" +
-			"\n" +
-			"First Chapter\n" +
-			"-------------\n" +
-			"\n" +
-			"This text is not on the cover page but the first chapter"
-				},
-				{kind: "Label", content:"Cover Page"}
-			]}
-		]}
+					"This text is not on the cover page but the first chapter"
+						},
+						{kind: "Label", content:"Cover Page"}
+					]}
+				]}
+			]
+		}
 	]
 });
