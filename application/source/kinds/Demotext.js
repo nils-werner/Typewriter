1f4363cab32dcfd92d003983fb9160f75ef86afc
Nils Werner <nils.werner@gmail.com>
2011-06-20 17:19:25 +0200

    Willkommensnachricht uebersetzbar

--- application/source/kinds/Demotext.js
+++ application/source/kinds/Demotext.js
@@ -2,45 +2,7 @@ enyo.kind({
 	name: "Demotext",
 	kind: enyo.Control,
 	published: {
-		text: ""+
-			"Typewriter\n"+
-			"==========\n"+
-			"\n"+
-			"Welcome to *Typewriter*, a Markdown editor for the HP TouchPad.\n"+
-			"\n"+
-			"Markdown\n"+
-			"--------\n"+
-			" \n"+
-			"Markdown lets you create richtext documents by entering text in a simple format that's easy to read and write. No bells and whistles, just you and your text.\n"+
-			"\n"+
-			"To create a document simply\n"+
-			"\n"+
-			" 1. Type Markdown text [in the editor][editor]\n"+
-			" 2. Preview the document after [dragging up the toolbar][preview]\n"+
-			" \n"+
-			"Markdown is a lightweight markup language based on the formatting conventions that people naturally use in email.  As [John Gruber] writes on the [Markdown site][markdown]:\n"+
-			"\n"+
-			"> The overriding design goal for Markdown's\n"+
-			"> formatting syntax is to make it as readable \n"+
-			"> as possible. The idea is that a\n"+
-			"> Markdown-formatted document should be\n"+
-			"> publishable as-is, as plain text, without\n"+
-			"> looking like it's been marked up with tags\n"+
-			"> or formatting instructions.\n"+
-			"\n"+
-			"This document is written in Markdown; you are currently seeing the preview. To get a feel for Markdown's syntax, *pull down the toolbar* and have a look.\n"+
-			"\n"+
-			"What next?\n"+
-			"----------\n"+
-			"\n"+
-			"Go right ahead, create a new document and start typing. And don't mind the style, just mind your words. It'll come out alright by itself.\n"+
-			"\n"+
-			"Once you've done a few lines you can sync the document back and forth with Dropbox so you can continue working with your text wherever you are.\n"+
-			"\n"+
-			"  [john gruber]: http://daringfireball.net/\n"+
-			"  [markdown]: http://daringfireball.net/projects/markdown/\n"+
-			"  [editor]: #editor\n"+
-			"  [preview]: #preview\n"+
-			"  [clear]: #clear"
-	}
+		text: $L({ value: this.value, key: "welcome"})
+	},
+	value: "Typewriter\n==========\n\nWelcome to *Typewriter*, a Markdown editor for the HP TouchPad.\n\nSimplicity\n----------\n\nMarkdown lets you create rich documents by entering text in a simple format that's easy to read and write. No bells and whistles, no buttons and wizards, just you and your text.\n\nTo create a document simply\n\n 1. Type Markdown text [in the editor][editor]\n 2. Preview the document by [dragging up the toolbar][preview]\n\nMarkdown\n--------\n\nMarkdown is a lightweight markup language based on the formatting conventions that people have grown accustomed from emails. The best thing about it is that even if you're unfamiliar with it it won't take more than a minute or two to get started with the basics.\n\nAs [John Gruber] writes on the [Markdown site][markdown]:\n\n> The overriding design goal for Markdown's\n> formatting syntax is to make it as readable \n> as possible. The idea is that a\n> Markdown-formatted document should be\n> publishable as-is, as plain text, without\n> looking like it's been marked up with tags\n> or formatting instructions.\n\nThis document is written in Markdown; you are currently seeing the preview. To get a feel for Markdown's syntax, *pull down the toolbar* and have a look.\n\nWhat next?\n----------\n\nGo right ahead, give it a whirl and start typing. This welcome-message is your sandbox, you can't break anything here.\n\nOnce you come up with a great idea for a story and want to actually save what you're writing, [create a new document][create].\n\nYou can also sync the document back and forth with Dropbox so you can continue working on your text wherever you are or share it with others with a single tap.\n\n  [john gruber]: http://daringfireball.net/\n  [markdown]: http://daringfireball.net/projects/markdown/\n  [editor]: #editor\n  [preview]: #preview\n  [clear]: #clear\n  [create]: #create"
 });
