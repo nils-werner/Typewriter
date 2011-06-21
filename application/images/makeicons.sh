91bc967275cbd6654a4ea511ef7b00b8b511a249
Nils Werner <nils.werner@gmail.com>
2011-06-21 16:04:57 +0200

    Auch 32px mit imagemagick

--- application/images/makeicons.sh
+++ application/images/makeicons.sh
@@ -3,3 +3,4 @@
 convert -resize 128x128 icon256.png icon128.png
 convert -resize 64x64 icon256.png icon64.png
 convert -resize 48x48 icon256.png icon48.png
+convert -resize 32x32 icon256.png icon32.png
