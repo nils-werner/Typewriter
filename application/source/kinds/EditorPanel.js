4861a39eeaa7788b3df8af59b36561cf11ef0110
Nils Werner <nils.werner@gmail.com>
2011-10-17 14:01:11 +0200

    Unnoetiges resizen raus, wird in 3.0.4 nicht benoetigt

--- application/source/kinds/EditorPanel.js
+++ application/source/kinds/EditorPanel.js
@@ -285,8 +285,8 @@ enyo.kind({
 		this.$.editorScroller.node.style.height = (s.h - 55 - kh) + "px";
 		
 		this.$.top.node.style.height = (s.h - 55 - kh) + "px";
-		this.$.bottom.node.style.height = (s.h - kh) + "px";
-		this.$.bottom.setPeekHeight(s.h - pcs.h);
+		//this.$.bottom.node.style.height = (s.h - kh) + "px";
+		//this.$.bottom.setPeekHeight(s.h - pcs.h);
 		
 		if(this.position == "up")
 			this.$.bottom.slidePosition = this.$.bottom.lastSlidePosition = -(enyo.fetchControlSize(this.$.bottom).h-56);
