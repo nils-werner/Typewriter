9c1a4339f5b4044f0ed54a2bb380e6fd84cb093e
Nils Werner <nils.werner@gmail.com>
2012-01-30 22:45:59 +0100

    Submodule and resource links for Web

--- application/depends.js
+++ application/depends.js
@@ -1,26 +1,26 @@
 /* Copyright 2009-2011 Hewlett-Packard Development Company, L.P. All rights reserved. */
 enyo.depends(
-	"http://demo.typewriterwebos.com/application/source/utilities/strings.js",
-	"http://demo.typewriterwebos.com/application/source/utilities/xslt.js",
-	"http://demo.typewriterwebos.com/application/source/showdown/compressed/showdown.js",
+	"/application/source/utilities/strings.js",
+	"/application/source/utilities/xslt.js",
+	"/application/source/showdown/compressed/showdown.js",
 	
-	"http://demo.typewriterwebos.com/application/source/App.js",
+	"/application/source/App.js",
 	
-	"http://demo.typewriterwebos.com/application/source/kinds/EditorPanel.js",
-	"http://demo.typewriterwebos.com/application/stylesheets/EditorPanel.css",
+	"/application/source/kinds/EditorPanel.js",
+	"/application/stylesheets/EditorPanel.css",
 	
-	"http://demo.typewriterwebos.com/application/source/kinds/HSlidingPane.js",
-	"http://demo.typewriterwebos.com/application/source/kinds/HSlidingView.js",
-	"http://demo.typewriterwebos.com/application/source/kinds/ScrollBarsScroller.js",
+	"/application/source/kinds/HSlidingPane.js",
+	"/application/source/kinds/HSlidingView.js",
+	"/application/source/kinds/ScrollBarsScroller.js",
 	
-	"http://demo.typewriterwebos.com/application/source/kinds/FileIO.js",
-	"http://demo.typewriterwebos.com/application/source/kinds/NewFileDialog.js",
-	"http://demo.typewriterwebos.com/application/source/kinds/LoginDialog.js",
-	"http://demo.typewriterwebos.com/application/source/kinds/ResolveDialog.js",
-	"http://demo.typewriterwebos.com/application/source/kinds/Demotext.js",
-	"http://demo.typewriterwebos.com/application/source/kinds/DragHint.js",
+	"/application/source/kinds/FileIO.js",
+	"/application/source/kinds/NewFileDialog.js",
+	"/application/source/kinds/LoginDialog.js",
+	"/application/source/kinds/ResolveDialog.js",
+	"/application/source/kinds/Demotext.js",
+	"/application/source/kinds/DragHint.js",
 	
-	"/printdialog/",
+	"/enyo/framework/lib/printdialog/",
 	
-	"http://demo.typewriterwebos.com/application/stylesheets/LaTeX.css"
+	"/application/stylesheets/LaTeX.css"
 );
